118a2592c12efead5aa14d81876fe8dc769e9449
Sofka <sofka.vs@gmail.com>
2019-11-25 23:07:04 -0600

    not finished

--- app.js
+++ app.js
@@ -9,69 +9,70 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 const render = require("./lib/htmlRenderer");
 const teamMembers = [];
 const idArray = [];
-const answers = [
-  {
-    type: "input",
-    name: "managerName",
-    message: "What is your manager's name?",
-    validate: answer => {
-      if (answer !== "") {
-        return true;
-      }
-      return "Please enter at least one character.";
-    }
-  },
-  {
-    type: "input",
-    name: "managerId",
-    message: "What is your manager's id?",
-    validate: answer => {
-      const pass = answer.match(/^[1-9]\d*$/);
-      if (pass) {
-        return true;
-      }
-      return "Please enter a positive number greater than zero.";
-    }
-  },
-  {
-    type: "input",
-    name: "managerEmail",
-    message: "What is your manager's email?",
-    validate: answer => {
-      const pass = answer.match(/\S+@\S+\.\S+/);
-      if (pass) {
-        return true;
-      }
-      return "Please enter a valid email address.";
-    }
-  },
-  {
-    type: "input",
-    name: "managerOfficeNumber",
-    message: "What is your manager's office number?",
-    validate: answer => {
-      const pass = answer.match(/^[1-9]\d*$/);
-      if (pass) {
-        return true;
-      }
-      return "Please enter a positive number greater than zero.";
-    }
-  }
-];
-//.then(answers => {
+
 function createManager() {
-  inquirer.prompt(answers).then(ans => {
-    console.log(ans);
-    const manager = new Manager(
-      ans.managerName,
-      ans.managerId,
-      ans.managerEmail,
-      ans.managerOfficeNumber
-    );
-    teamMembers.push(manager);
-    idArray.push(answers.managerId);
-    createTeam();
-  });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is your manager's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is your manager's id?",
+        validate: answer => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your manager's email?",
+        validate: answer => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is your manager's office number?",
+        validate: answer => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      }
+    ])
+    .then(answer => {
+      console.log(answer);
+      const manager = new Manager(
+        answer.managerName,
+        answer.managerId,
+        answer.managerEmail,
+        answer.managerOfficeNumber
+      );
+      teamMembers.push(manager);
+      idArray.push(answer.managerId);
+      createTeam();
+    });
 }
 createManager();
 
@@ -156,15 +157,15 @@ function addIntern() {
         }
       }
     ])
-    .then(answers => {
+    .then(answer => {
       const intern = new Intern(
-        answers.internName,
-        answers.internId,
-        answers.internEmail,
-        answers.internSchool
+        answer.internName,
+        answer.internId,
+        answer.internEmail,
+        answer.internSchool
       );
       teamMembers.push(intern);
-      idArray.push(answers.internId);
+      idArray.push(answer.internId);
       createTeam();
     });
 }
@@ -219,15 +220,15 @@ function addEngineer() {
         }
       }
     ])
-    .then(answers => {
+    .then(answer => {
       const engineer = new Engineer(
-        answers.engineerName,
-        answers.engineerId,
-        answers.engineerEmail,
-        answers.engineerGithub
+        answer.engineerName,
+        answer.engineerId,
+        answer.engineerEmail,
+        answer.engineerGithub
       );
       teamMembers.push(engineer);
-      idArray.push(answers.engineerId);
+      idArray.push(answer.engineerId);
       createTeam();
     });
 }
